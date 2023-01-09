aad958434edb68d56bd7b1fd764c3a0b3c0e92a0
nicompiledev <nsrivera80@misena.edu.co>
2023-01-08 23:09:47 -0500

    validate email with a regular expression

--- js/app.js
+++ js/app.js
@@ -1,53 +1,69 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", function () {
+  //Seleccionar los elementos de la interfaz
+  const inputEmail = document.querySelector("#email");
+  const inputAsunto = document.querySelector("#asunto");
+  const inputMensaje = document.querySelector("#mensaje");
+  const formulario = document.querySelector("#formulario");
 
-    //Seleccionar los elementos de la interfaz
-    const inputEmail = document.querySelector('#email');
-    const inputAsunto = document.querySelector('#asunto');
-    const inputMensaje = document.querySelector('#mensaje');
-    const formulario = document.querySelector('#formulario');
+  //Asignar un evento a cada input
+  inputEmail.addEventListener("blur", validar);
 
-    //Asignar un evento a cada input
-    inputEmail.addEventListener('blur', validar);
+  inputAsunto.addEventListener("blur", validar);
 
-    inputAsunto.addEventListener('blur', validar);
+  inputMensaje.addEventListener("blur", validar);
 
-    inputMensaje.addEventListener('blur', validar);
-
-    function validar(e) {
-        //Validar que el campo no este vacio
-        if(e.target.value.trim() === ''){
-            mostrarAlerta(`El campo ${e.target.id} es obligatorio`, e.target.parentElement);
-            return;    
-        }
-        limpiarAlerta(e.target.parentElement);
-        console.log('despues del if');
+  function validar(e) {
+    //Validar que el campo no este vacio
+    if (e.target.value.trim() === "") {
+      mostrarAlerta(
+        `El campo ${e.target.id} es obligatorio`,
+        e.target.parentElement
+      );
+      return;
     }
 
-    function mostrarAlerta(mensaje, referencia) {
-        //Comprobar si ya existe una alerta
-        const alerta = referencia.querySelector('.bg-red-600');
-        console.log(alerta);
-        if(alerta){
-            alerta.remove();
-        }
-
-        //Generar alerta en HTML
-        const error = document.createElement('p');
-        error.textContent = mensaje;
-        error.classList.add('bg-red-600', 'text-white',  'p-3','rounded', 'text-xs', 'font-bold', 'text-center', 'mt-3');
-
-        
-        //Insertar en el HTML
-        referencia.appendChild(error);
+    if (e.target.id === 'email' && !validarEmail(e.target.value)) {
+      mostrarAlerta("Email no valido", e.target.parentElement); 
+      return;     
     }
+    limpiarAlerta(e.target.parentElement);
+  }
 
-    function limpiarAlerta(referencia) {
-        const alerta = referencia.querySelector('.bg-red-600');
-        if(alerta){
-            alerta.remove();
-        }
+  function mostrarAlerta(mensaje, referencia) {
+    limpiarAlerta(referencia);
+
+    //Generar alerta en HTML
+    const error = document.createElement("p");
+    error.textContent = mensaje;
+    error.classList.add(
+      "bg-red-600",
+      "text-white",
+      "p-3",
+      "rounded",
+      "text-xs",
+      "font-bold",
+      "text-center",
+      "mt-3"
+    );
+
+    //Insertar en el HTML
+    referencia.appendChild(error);
+  }
+
+  function limpiarAlerta(referencia) {
+    //Comprobar si ya existe una alerta
+    const alerta = referencia.querySelector(".bg-red-600");
+    if (alerta) {
+      alerta.remove();
     }
+  }
 
+  function validarEmail(email) {
+    const expReg =
+      /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+
+    const resultado = expReg.test(email);
+    console.log(resultado)  
+    return expReg.test(email);
+  }
 });
-
-
